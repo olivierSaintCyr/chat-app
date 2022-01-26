775cb5994576c0830cc28414387657aefe8f91e0
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2022-01-26 02:14:00 -0500

    fix(message): order by sent date

--- server/chat-server/app/db-services/messages-db.service.ts
+++ server/chat-server/app/db-services/messages-db.service.ts
@@ -2,6 +2,7 @@ import { CassandraDBClient } from '@app/db-services/cassandra-db-client.service'
 import { Message, MessageType } from '@app/message.interface';
 import { Service } from 'typedi';
 
+export const MAX_MESSAGES_PER_QUERY = 20;
 @Service()
 export class MessagesDBService {
 
@@ -15,23 +16,54 @@ export class MessagesDBService {
         this.client.execute(query).then((res) => console.log(res));
     }
 
+    async getLastestMessages(conversationId: string) {
+        const query = `SELECT author, content, sent_date
+            FROM chat_data.message
+            WHERE conversation = ${conversationId}
+            ORDER BY sent_date DESC
+            LIMIT ${MAX_MESSAGES_PER_QUERY};
+        `;
+        return await this.getMessagesWithQuery(query);
+    }
+
+    async getLastestMessagesFrom(conversationId: string, date: Date) {
+        const query = `SELECT author, content, sent_date
+            FROM chat_data.message
+            WHERE conversation = ${conversationId}
+            AND sent_date < ${date.getTime()}
+            ORDER BY sent_date DESC
+            LIMIT ${MAX_MESSAGES_PER_QUERY};
+        `;
+        return await this.getMessagesWithQuery(query);
+    }
+
     async getMessage(messageId: string): Promise<Message> {
         const query = `SELECT author, content, conversation, date 
             FROM chat_data.message 
             WHERE id = ${messageId};`;
+        const messages = await this.getMessagesWithQuery(query);
+        if (messages.length === 0) {
+            throw Error(`No message with id: ${messageId}`);
+        }
+        return messages[0];
+    }
+
+    private async getMessagesWithQuery(query: string) {
         const result = await this.client.execute(query);
         if (result.rows.length === 0) {
-            throw Error(`No message found with id: ${messageId}`);
+            return [] as Message[];
         }
-        const messageRow = result.rows[0];
-        const message: Message = {
-            conversation: messageRow.conversation,
-            author: messageRow.author,
-            content: messageRow.content,
-            type: MessageType.Text, // TODO change for image suport 
-            date: messageRow.date
-        };
-        return message;
+        const messages: Message[] = result.rows.map((row) => {
+            const message: Message = {
+                author: row.author,
+                conversation: row.conversation,
+                content: row.content,
+                type: MessageType.Text,
+                date: row.sent_date,
+            }
+            return message;
+        });
+        return messages;
     }
 
     private createNewMessageQuery(message: Message) {
