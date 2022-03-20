4351daf7f56fd4f0d639ce3e4b3bb49ed80af8d8
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2022-03-20 00:10:08 -0400

    feat(image-server): Profile picture controller

--- server/image-server/app/index.ts
+++ server/image-server/app/index.ts
@@ -9,7 +9,8 @@ import dotenv from 'dotenv';
 import { AuthService } from '@app/auth/auth.service';
 import { UsersAccessService } from '@app/users/users-access.service';
 import { spawn } from 'child_process';
-import { upload } from 'multer';
+import { ProfilePictureService } from '@app/image-services/profile-picture.service';
+import { ProfilePictureController } from '@app/image-services/profile-picture-controller';
 
 dotenv.config({ path: path.join(__dirname, `./.env`)});
 
@@ -24,13 +25,17 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World! welcome to user api');
+    res.send('Hello World! Welcome to image api');
 });
 const authService = Container.get(AuthService);
 app.use(authService.middleware);
 
 const userAccessService = Container.get(UsersAccessService);
 app.use(userAccessService.middleware);
+
+const profilePictureService = Container.get(ProfilePictureService);
+const profilePictureController = new ProfilePictureController(profilePictureService);
+app.use('/profile-pictures', profilePictureController.router);
 
 const PORT = 8084;
 app.listen(PORT, () => console.log(`Conversation api is listenning on port: ${PORT}`));
