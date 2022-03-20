911919f124daa77da0ffd29803b62154a596feef
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2022-03-20 00:38:18 -0400

    feat(profile-picture-controller)(image-server): upload and fetch of profile picture

--- server/image-server/app/image-services/profile-picture-controller.ts
+++ server/image-server/app/image-services/profile-picture-controller.ts
@@ -1,25 +1,34 @@
 import { ProfilePictureService } from '@app/image-services/profile-picture.service';
+import { PROFILE_PICTURE_UPLOAD_DIR_PATH } from '@app/path';
 import { Router } from 'express';
+import multer from 'multer';
+
 
 export class ProfilePictureController {
     readonly router = Router();
+
+    private upload = multer({ dest: PROFILE_PICTURE_UPLOAD_DIR_PATH });
 
     constructor(private profilePictureService: ProfilePictureService) {
         this.setRoutes();
     }
 
     private setRoutes() {
-        this.router.get('/:imageId', (req, res) => {
+        this.router.get('/:imageId', async (req, res) => {
             const { imageId } = req.params;
             if (imageId === undefined) {
                 return res.sendStatus(400);
             }
-            console.log(imageId);
-            return res.sendStatus(200);
+            const readStream = await this.profilePictureService.getPicture(imageId);
+            readStream.pipe(res);
         });
 
-        this.router.post('/', async (req, res) => {
-            const filePath = './data/images/1_mk1-6aYaf_Bes1E3Imhc0A.jpeg';
+        this.router.post('/', this.upload.single('image'), async (req, res) => {
+            const { file } = req;
+            if (file === undefined) {
+                return res.sendStatus(400);
+            }
+            const filePath = (file as Express.Multer.File).path;
             const imageId = await this.profilePictureService.uploadPicture(filePath);
             return res.send({ imageId });
         });
