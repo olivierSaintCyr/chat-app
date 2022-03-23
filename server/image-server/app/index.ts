e100b9200940095995b8abf5a95c4fffc9a6818f
olivierSaintCyr <60223659+olivierSaintCyr@users.noreply.github.com>
2022-03-22 23:39:06 -0400

    adding image server to docker compose

--- server/image-server/app/index.ts
+++ server/image-server/app/index.ts
@@ -1,17 +1,14 @@
 import 'module-alias/register';
 import 'reflect-metadata';
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { Container } from 'typedi';
-import path from 'path';
-import dotenv from 'dotenv';
 import { AuthService } from '@app/auth/auth.service';
 import { UsersAccessService } from '@app/users/users-access.service';
 import { ProfilePictureService } from '@app/image-services/profile-picture.service';
 import { ProfilePictureController } from '@app/image-services/profile-picture-controller';
-
-dotenv.config({ path: path.join(__dirname, `./.env`)});
 
 const app = express();
 app.use(express.json({ limit: '300mb' }));
@@ -33,5 +30,5 @@ const profilePictureService = Container.get(ProfilePictureService);
 const profilePictureController = new ProfilePictureController(profilePictureService);
 app.use('/profile-pictures', profilePictureController.router);
 
-const PORT = 8084;
-app.listen(PORT, () => console.log(`Conversation api is listenning on port: ${PORT}`));
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Image server is listenning on port: ${PORT}`));
